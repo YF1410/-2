0f6eb9fbc90646c30ab3a4e7408ce7b1835659b2
LE2B_12_YugoKuriyama <k020g1224@g.neec.ac.jp>
2021-11-10 15:26:52 +0900

    プレイヤー描画とカメラ追従

--- prog01/prog01/WinApp.cpp
+++ prog01/prog01/WinApp.cpp
@@ -1,6 +1,6 @@
 #include "WinApp.h"
 
-const wchar_t WinApp::windowClassName[] = L"DangerousWalk";
+const wchar_t WinApp::windowClassName[] = L"Impact";
 
 LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
